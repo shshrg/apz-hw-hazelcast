489e53f1123700838724f067bfd37da7037e69d0
oleksandra <sasha.shergina@gmail.com>
2026-03-01 20:45:54 +0200

    Created initial project

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
